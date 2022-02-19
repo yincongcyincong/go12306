dc200f6254d79f50bb3542f647a78cdbb7a70c0d
yincong <yincong@baidu.com>
2022-02-19 14:02:07 +0800

    修改无座

--- conf/conf.go
+++ conf/conf.go
@@ -6,7 +6,7 @@ var (
 	Station = map[string]string{}
 
 	OrderSeatType = map[string]string{
-		"特等座": "P", "商务座": "9", "一等座": "M", "二等座": "O", "软卧": "4", "硬卧": "3", "硬座": "1", "无座": "1",
+		"特等座": "P", "商务座": "9", "一等座": "M", "二等座": "O", "软卧": "4", "硬卧": "3", "硬座": "1",
 	}
 	PassengerType = map[string]string{
 		"成人":          "ADULT",
