4848f50a2b44cdf903dc4eb195ebf95e2cb2de48
yincong <yincong@baidu.com>
2022-03-03 17:03:45 +0800

    增加打包脚本

---
+++ build.sh
@@ -0,0 +1,6 @@
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o go12306-linux
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o go12306-windows.exe
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o go12306-mac
+
+tar -zcvf go12306.tar.gz ./go12306-linux ./go12306-windows.exe ./go12306-mac
+rm -rf go12306-linux go12306-windows.exe go12306-mac
